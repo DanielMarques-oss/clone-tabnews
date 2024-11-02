58ca686ad4f4d769ac2eae7c617681361ecef79d
DanielMarques-oss <danmarq.analytics@gmail.com>
2024-11-02 17:53:08 +0000

    make database.js client.connect more robust to error

--- infra/database.js
+++ infra/database.js
@@ -8,19 +8,20 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-
-  await client.connect();
-  if (client._connected) {
-    console.log("Conexão estabelecida com sucesso");
-  } else {
-    console.log("Falha ao estabelecer conexão");
-  }
-
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
     console.error(err);
+    throw err;
   } finally {
     await client.end();
   }
